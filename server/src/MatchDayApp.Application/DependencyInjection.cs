bf74bc80cbc260394ae09b910657de0d050f8afe
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-11-01 00:42:00 -0300

    Criação da injeção de dependencia das camada de infra e aplicação. Configurando swagger e jwt

---
+++ server/src/MatchDayApp.Application/DependencyInjection.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using MatchDayApp.Application.Interfaces;
+using MatchDayApp.Application.Services;
+using MatchDayApp.Domain.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MatchDayApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            services.AddAutoMapper(Assembly.Load("MatchDayApp.Application"));
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<ITeamService, TeamService>();
+            services.AddTransient<ISoccerCourtService, SoccerCourtService>();
+            services.AddTransient<IScheduleMatchService, ScheduleMatchService>();
+
+            services.AddJwt(configuration);
+            services.AddSwagger(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            var swaggerOptions = new SwaggerOptions();
+            configuration.Bind(nameof(SwaggerOptions), swaggerOptions);
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MatchDayApp API", Version = "v1" });
+
+                opt.ExampleFilters();
+
+                var security = new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                };
+
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                opt.AddSecurityRequirement(security);
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                opt.IncludeXmlComments(xmlPath);
+            });
+
+            // Criar os exemplos que foram criados na pasta SwaggerExamples na documentação
+            services.AddSwaggerExamplesFromAssemblyOf(typeof(DependencyInjection));
+
+            return services;
+        }
+
+        private static IServiceCollection AddJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtOptions = new JwtOptions();
+            configuration.Bind(nameof(JwtOptions), jwtOptions);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtOptions.Secret)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = true,
+                ValidateLifetime = true
+            };
+
+            services.AddSingleton(jwtOptions);
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.SaveToken = true;
+                x.TokenValidationParameters = tokenValidationParameters;
+            });
+
+            return services;
+        }
+    }
+}
