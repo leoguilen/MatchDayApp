a5d1bbfbca7fcae79fedadadd52a2388077fcad5
Leonardo Guilen <leonardo.guilen@outlook.com.br>
2021-01-06 15:44:59 -0300

    finalizando montagem da home page

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,11 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import './styles/main.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
--- web/src/pages/home/Home.vue
+++ web/src/pages/home/Home.vue
@@ -4,11 +4,12 @@
       <nav class="navbar">
         <div>
           <a class="navbar-brand" href="#">
-            <img class="d-inline-block align-top" src="https://media.eadbox.com/system/uploads/medium/file/5d3cab6c0e3b5000422bc268/logo%20edools%20testehk2.png" alt="Logo" height="50">
+            <img class="d-inline-block align-top" src="../../assets/images/logo.png" alt="Logo" height="50">
           </a>
         </div>
         <div class="header-nav-bar-links">
           <a href="#">Suporte</a>
+          <a href="#">Baixar</a>
           <span style="font-weight: 300">|</span>
           <a href="#">Inscrever-se</a>
           <a href="#">Entrar</a>
@@ -27,10 +28,58 @@
           <p style="font-size: 13px; margin-top: 2.5rem; margin-bottom: -1rem">O uso desse aplicativo é totalmente isento de custos. Para mais informações consulte os Termos e Condições de Uso.</p>
         </div>
         <div class="col-6">
-          <img height="500" src="https://th.bing.com/th/id/Rf310137ff80cddb1010ecd94f844ac76?rik=rTuDjO3j8%2fjZMQ&riu=http%3a%2f%2fwww.freepngclipart.com%2fdownload%2ffootball%2f84165-vector-player-football-players-free-frame.png&ehk=1WpfSdIjFPuVXRQeVw%2f9Hb69MIKldN%2fvUpE2UrKQS8o%3d&risl=&pid=ImgRaw" alt="">
+          <img height="500" src="" alt="Futebol">
         </div>
       </div>
     </section>
+    <section class="section2">
+      <div class="row">
+        <div class="col-6">
+          <img height="500" src="../../assets/images/mobile.png" alt="Smartphone">
+        </div>
+        <div class="col-6 text-right">
+          <p style="font-size: 15px; letter-spacing: 2px"><b>MATCH DAY APP MOBILE</b></p>
+          <h1 style="font-size: 60px"><b>Aplicativo dispónivel para download</b></h1>
+          <p style="letter-spacing: 0.5px;font-weight: 100;"><b>Marque suas partidas aonde estiver com apenas um <br> toque na tela.</b></p>
+          <button type="button">Baixe agora o app</button>
+        </div>
+      </div>
+    </section>
+    <footer class="text-center text-lg-start">
+      <div class="text-center p-3">
+        <div class="footer-logo">
+          <a class="navbar-brand" href="#">
+            <img class="d-inline-block align-top" src="../../assets/images/logo.png" alt="Logo" height="50">
+          </a>
+        </div>
+      </div>
+      <div class="text-center">
+        <div class="footer-socialmedias">
+          <a href="https://www.instagram.com/leocruzg_" target="_blank">
+            <span><i class="fab fa-instagram"></i></span>
+          </a>
+          <a href="https://www.linkedin.com/in/leonardo-guilen/" target="_blank">
+            <span><i class="fab fa-linkedin"></i></span>
+          </a>
+          <a href="https://github.com/leoguilen" target="_blank">
+            <span><i class="fab fa-github"></i></span>
+          </a>
+        </div>
+      </div>
+      <div class="text-center p-3">
+        <div class="footer-links">
+          <a href="#" target="_blank">Sobre</a>
+          <a href="https://github.com/leoguilen/matchday-app">Source code</a>
+          <a href="#" target="_blank">Inscrever-se</a>
+          <a href="#" target="_blank">Baixar</a>
+        </div>
+      </div>
+      <div class="text-center p-3">
+        <div class="footer-copyright">
+          © 2021 Copyright: MatchDayApp Team
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -65,7 +114,7 @@ header {
     text-decoration: none;
     margin: 0 2.5rem 0 0;
   }
-  .header-nav-bar-links a:nth-child(3){
+  .header-nav-bar-links a:nth-child(4){
     margin-left: 2.5rem;
     color: #CBDADC;
   }
@@ -108,4 +157,73 @@ section {
     color: black;
   }
 }
+.section2 {
+  background: #2D46B9;
+  color: #1ED760;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+
+  p {
+    font-size: 17px;
+  }
+
+  button {
+    width: 18rem;
+    height: 2.7rem;
+    border-radius: 20px;
+    background: #1ED760;
+    color: #2D46B9;
+    font-weight: 700;
+    letter-spacing: 1px;
+    margin-top: 20px;
+  }
+
+  button:hover {
+    background: white;
+    color: black;
+  }
+}
+
+footer {
+  width: 100vw;
+  padding: 2rem 15rem 2rem 16rem;
+  background: var(--header);
+  color: var(--white);
+
+  .footer-socialmedias {
+    margin-left: 2.5rem;
+  }
+
+  .footer-links {
+    margin-left: 2.5rem;
+  }
+
+  .footer-socialmedias a > span{
+    color: var(--white);
+    font-size: 2.5em;
+    text-decoration: none;
+    margin: 0 2.5rem 0 0;
+  }
+
+  .footer-socialmedias a span:hover{
+    color: #1DB954;
+  }
+
+  .footer-links > a {
+    color: var(--white);
+    text-decoration: none;
+    margin: 0 2.5rem 0 0;
+  }
+
+  .footer-links a:hover{
+    color: #1DB954;
+  }
+
+  .footer-copyright {
+    font-size: 0.8rem;
+    letter-spacing: 1.2px;
+    opacity: 0.75;
+  }
+}
+
 </style>
